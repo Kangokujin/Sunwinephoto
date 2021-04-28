7c90abed18245f89fdb2b2e9a215a3b8164b36b8
Kangokujin <lkun.tv@gmail.com>
2021-04-28 16:30:47 +0500

    PoFFiksi BaGi

--- main/urls.py
+++ main/urls.py
@@ -3,7 +3,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='main'),
     path('about-me', views.about, name='about'),
-    path('portfolio', views.portfolio, name='portfolio'),
-    path('blog', views.blog, name='blog'),
+    path('single', views.single, name='single'),
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
     path('category/<str:category>/', views.CategoryListView.as_view())
 ]
--- main/views.py
+++ main/views.py
@@ -19,12 +19,12 @@ def about(request):
     return render(request, 'main/about.html')
 
 
-def portfolio(request):
-    return render(request, 'main/portfolio.html')
+def single(request):
+    return render(request, 'main/single.html')
 
 
-def blog(request):
-    return render(request, 'main/blog.html')
+def services(request):
+    return render(request, 'main/services.html')
 
 
 class CategoryListView(View):
